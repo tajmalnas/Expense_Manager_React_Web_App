22667dc63390f92e02e47635dbd1bac6b84e2c08
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-13 23:52:48 +0530

    made basic list .learned components and props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ExpenseItem from './components/ExpenseItem'
+import React from 'react';
+
+import Expenses from './components/Expense/Expenses';
 
 const App = () => {
   const expenses = [
@@ -23,14 +24,12 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expenses items={expenses} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
