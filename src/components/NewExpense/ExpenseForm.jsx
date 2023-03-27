8b3eb55b73c599783a7ae117d668f954dddd45da
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-27 23:04:18 +0530

    deployed this app

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -44,11 +44,11 @@ const ExpenseForm = (props) => {
 
     const submitHandler=(event)=>{
         event.preventDefault();
-        const expenseData={
-            title:enteredTitle,
-            amount:enteredAmount,
-            date:new Date(enteredDate)
-        }
+        const expenseData = {
+            title: enteredTitle,
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
         props.onSaveExpenseData(expenseData);
         setEnteredAmount('');
         setEnteredDate('');
